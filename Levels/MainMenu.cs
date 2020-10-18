4c3e6268f44d4ade37b6c7df521a884c82a4cb14
sitiom <56180050+sitiom@users.noreply.github.com>
2020-10-18 21:13:08 +0800

    Initial commit

---
+++ Levels/MainMenu.cs
@@ -0,0 +1,12 @@
+using Godot;
+
+namespace Labyrinth.Levels
+{
+	public class MainMenu : Node2D
+	{
+		public override void _Ready()
+		{
+		
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Labyrinth
+A horror game made in godot.
+
+## Prerequisites
+- .NET Framework `4.8` & Godot `3.2.3` Mono.
