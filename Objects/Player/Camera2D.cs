d7341e4ecadb9edb34f8493803a00a4455516d17
Johndayll <johndayllearizala@yahoo.com>
2020-10-19 11:55:10 +0800

    Add player movement

---
+++ Objects/Player/Camera2D.cs
@@ -0,0 +1,20 @@
+using Godot;
+using System;
+
+namespace Debugmancer.Objects.Player
+{
+    public class Camera2D : Godot.Camera2D
+    {
+        public override void _Ready()
+        {
+            SetAsToplevel(true);
+		    SetProcess(false);
+        }
+        public override void _PhysicsProcess(float delta)
+        {
+            Position = new Vector2(
+				    (-((GetParent<KinematicBody2D>().Position.x - GetGlobalMousePosition().x) / 2 * .6f) + GetParent<KinematicBody2D>().GlobalPosition.x), 
+				    (-((GetParent<KinematicBody2D>().Position.y - GetGlobalMousePosition().y) / 2 * .6f) + GetParent<KinematicBody2D>().GlobalPosition.y));
+        }
+    }
+}
