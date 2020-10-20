215a65b9a25b22d7760f464d2ae94b06389c0f42
sitiom <56180050+sitiom@users.noreply.github.com>
2020-10-20 10:14:37 +0000

    Update Torch.cs

--- Objects/Torch.cs
+++ Objects/Torch.cs
@@ -1,21 +1,12 @@
 using Godot;
-using System;
 
-public class Torch : Sprite
+namespace Labyrinth.Objects
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
-
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-        GetNode<AnimationPlayer>("AnimationPlayer").Play("burn");
-    }
-
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+	public class Torch : Sprite
+	{
+		public override void _Ready()
+		{
+			GetNode<AnimationPlayer>("AnimationPlayer").Play("burn");
+		}
+	}
 }
